945da9de066fd11275acb4b7ecef29aa1f11133e
Darya Radzevich <ganocij1@gmail.com>
2020-12-03 20:41:40 +0300

    fix: fix bug

--- action.js
+++ action.js
@@ -1,17 +1,26 @@
 const speed = document.querySelector(".speed");
 const bar = speed.querySelector(".speed-bar");
 const video = document.querySelector(".video");
+let speedDown = false;
 
 function handleMove(e) {
+  if (!speedDown) return;
   const y = e.pageY - this.offsetTop;
   const percent = y / this.offsetHeight;
   const min = 0.4;
   const max = 4;
   const height = Math.round(percent * 100) + "%";
   const playbackRate = percent * (max - min) + min;
-  bar.style.height = height; 
+  bar.style.height = height;
   bar.textContent = playbackRate.toFixed(2) + "×"; //0.00
   video.playbackRate = playbackRate; //speed 1.0
 }
 
-speed.addEventListener("mousemove", handleMove); //click  mousedown
+speed.addEventListener("mousemove", handleMove); 
+speed.addEventListener("mousedown", () => {
+  speedDown = true;
+});
+
+speed.addEventListener("mouseup", () => {
+  speedDown = false;
+});
